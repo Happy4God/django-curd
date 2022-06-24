d36ece7c42c775a67a30cdb2d6e089370290e9b0
Happy4God <105531051+Happy4God@users.noreply.github.com>
2022-06-24 03:53:27 +0100

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -5,22 +5,27 @@ from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.detail import DetailView
 from .models import Post
 
+
 # Create your views here.
 class PostListView(ListView):
-    model = Post 
+    model = Post
+
 
 class PostCreateView(CreateView):
     model = Post
     fields = "__all__"
     success_url = reverse_lazy("blog:all")
 
+
 class PostDetailView(DetailView):
     model = Post
+
 
 class PostUpdateView(UpdateView):
     model = Post
     fields = "__all__"
     success_url = reverse_lazy("blog:all")
+
 
 class PostDeleteView(UpdateView):
     model = Post
